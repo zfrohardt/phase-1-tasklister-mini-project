9ce432ca0d5aa718b3b83880b041030a4b9179f4
Zach Frohardt <zfrohardt@gmail.com>
2021-04-07 11:41:17 -0700

    finished minimum deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    document.getElementById("create-task-form").addEventListener("submit", submitButtonHandler);
 });
+
+let submitButtonHandler = event => {
+    event.preventDefault();
+    let taskInput = document.getElementById("new-task-description");
+    if (taskInput.value === "") {
+        return;
+    }
+
+    let task = document.createElement("li");
+    task.innerHTML = taskInput.value;
+    taskInput.value = "";
+
+    let taskList = document.getElementById("tasks");
+    taskList.appendChild(task);
+}
